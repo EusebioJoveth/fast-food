8948bb270a8c96899c13b8e08a4af1e44b1cc4bf
Eusebio Joveth <yurijoveth@gmail.com>
2022-05-13 16:37:00 +0100

    install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'jovethtecnologia-d15a2',
+    appId: '1:90905529948:web:1a7cb781b51bc03365a099',
+    databaseURL: 'https://jovethtecnologia-d15a2-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'jovethtecnologia-d15a2.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAlTmVz_oH6uvBO7bIpcBnFPi2rhi1wxMA',
+    authDomain: 'jovethtecnologia-d15a2.firebaseapp.com',
+    messagingSenderId: '90905529948',
+    measurementId: 'G-LPMSD236GJ',
+  },
   production: false
 };
 
