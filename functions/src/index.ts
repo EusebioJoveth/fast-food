a3d3d4cce9a3dcd8c1557558a5e49242379a6857
Eusebio Joveth <eusebio.joveth@porlande.com>
2022-09-23 13:51:42 +0100

    implementando cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,8 @@
 import * as functions from "firebase-functions";
-import * as admin from 'firebase-admin';
-admin.initializeApp();
 
- export const createConatctPr = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore().collection('users').add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `User Adicionado com ID: ${writeResult.id} added.`});
-});
+export const createUpdateUser = functions.firestore
+    .document("users/{id}").onWrite((change, context) => {
+      console.log("change", change, "==", "context", context);
+    });
+
+
